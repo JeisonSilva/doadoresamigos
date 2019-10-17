78d5a8a6d8fb5eba8465d57323b54f88dad72004
Jeison O. da Silva <jeison.desenvolvimento@gmail.com>
2019-10-16 23:09:34 -0300

    Feature/exclusao doador (#6)
    * criado testes para exclusao de doadores
    
    * incluido status release na aplicacao
    
    * alterado versao do aplicativo

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # doadoresamigos
+
+### Status Release
+[![Build status](https://build.appcenter.ms/v0.1/apps/354f4faa-db98-404e-a053-474d797160f6/branches/master/badge)](https://appcenter.ms)
--- app/src/main/java/com/jsonapp/doadoresamigos/gestaodoadores/ExclusaoDoador.java
+++ app/src/main/java/com/jsonapp/doadoresamigos/gestaodoadores/ExclusaoDoador.java
@@ -1,4 +1,29 @@
 package com.jsonapp.doadoresamigos.gestaodoadores;
 
 public class ExclusaoDoador {
+    private final ExclusaoDoadorDal exclusaoDoadorDal;
+    private final DoadorRepositorio doadorRepositorio;
+
+    public ExclusaoDoador(ExclusaoDoadorDal exclusaoDoadorDal, DoadorRepositorio doadorRepositorio) {
+        this.exclusaoDoadorDal = exclusaoDoadorDal;
+        this.doadorRepositorio = doadorRepositorio;
+    }
+
+    public void excluirDoador() {
+        Integer codDoador = this.exclusaoDoadorDal.obterCodigoDoador();
+        DoadorDto doadorDto = this.doadorRepositorio.pesquisarPorCodigo(codDoador);
+
+        if(codDoador == 0){
+            this.exclusaoDoadorDal.notificarCodigoInvalido();
+            return;
+        }
+
+        if(doadorDto == null){
+            this.exclusaoDoadorDal.notificarUsuarioInexistente();
+            return;
+        }
+
+        this.doadorRepositorio.excluir(doadorDto);
+
+    }
 }
