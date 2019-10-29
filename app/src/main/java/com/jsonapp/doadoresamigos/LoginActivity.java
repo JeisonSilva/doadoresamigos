fc7561ad1aeb3c0796d86014c937e2a84fea8440
Jeison O. da Silva <jeison.desenvolvimento@gmail.com>
2019-10-29 00:19:39 -0300

    criado tela login e implementado retorno de dados pela tela

--- app/src/main/java/com/jsonapp/doadoresamigos/LoginActivity.java
+++ app/src/main/java/com/jsonapp/doadoresamigos/LoginActivity.java
@@ -1,14 +1,63 @@
 package com.jsonapp.doadoresamigos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.jsonapp.doadoresamigos.autenticacao.AutenticacaoDal;
+import com.jsonapp.doadoresamigos.autenticacao.ContaDto;
+import com.jsonapp.doadoresamigos.gestaodoadores.DoadorDto;
+
+public class LoginActivity extends AppCompatActivity implements AutenticacaoDal {
+
+    private TextInputLayout inputTextUsuario;
+    private TextInputEditText inputEditUsuario;
+    private TextInputLayout inputTextSenha;
+    private TextInputEditText inputEditSenha;
+    private AppCompatButton btnInicioSessao;
+    private AppCompatButton btnNovoDoador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        inputTextUsuario = findViewById(R.id.inputTextUsuario);
+        inputEditUsuario = findViewById(R.id.inputEditUsuario);
+        inputTextSenha = findViewById(R.id.inputTextSenha);
+        inputEditSenha = findViewById(R.id.inputEditSenha);
+
+        btnInicioSessao = findViewById(R.id.btnInicioSessao);
+        btnNovoDoador = findViewById(R.id.btnNovoUsuario);
+    }
+
+    @Override
+    public ContaDto obterDadosUsuario() {
+        String usuario = String.valueOf(inputEditUsuario.getText());
+        String senha = String.valueOf(inputEditSenha.getText());
+        ContaDto contaDto= new ContaDto();
+
+        contaDto.setUsuario(usuario);
+        contaDto.setSenha(senha);
+        return contaDto;
+    }
+
+    @Override
+    public void exibirPesquisaDoadores() {
+
+    }
+
+    @Override
+    public void notificarUsuarioInvalido() {
+
+    }
+
+    @Override
+    public void notificarSenhaInvalida() {
+
     }
 }
