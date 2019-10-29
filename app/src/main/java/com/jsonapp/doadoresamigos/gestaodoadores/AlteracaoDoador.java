19ecba3a06adc1a1ee4c81a85ede3fdba18a3ea2
Jeison O. da Silva <jeison.desenvolvimento@gmail.com>
2019-10-28 23:42:12 -0300

    Feature/alteracao dados doadores (#8)
    * alterado versao do aplicativo
    
    * criado testes para alteracao de dados do doador

--- app/src/main/java/com/jsonapp/doadoresamigos/gestaodoadores/AlteracaoDoador.java
+++ app/src/main/java/com/jsonapp/doadoresamigos/gestaodoadores/AlteracaoDoador.java
@@ -1,4 +1,15 @@
 package com.jsonapp.doadoresamigos.gestaodoadores;
 
 public class AlteracaoDoador {
+    private final AlteracaoDoadorDal doadorDal;
+    private final DoadorRepositorio doadorRepositorio;
+
+    public AlteracaoDoador(AlteracaoDoadorDal doadorDal, DoadorRepositorio doadorRepositorio) {
+        this.doadorDal = doadorDal;
+        this.doadorRepositorio = doadorRepositorio;
+    }
+
+    public void alterar() {
+
+    }
 }
