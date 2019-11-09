1e070170d596866af65793250a1e672b636630c6
Jeison O. da Silva <jeison.desenvolvimento@gmail.com>
2019-11-09 01:56:14 -0300

    Feature/deletar doador (#12)
    * implementado exclusao de doadores
    
    * alterado versao

--- app/src/main/java/com/jsonapp/doadoresamigos/adapters/DoadorAdapter.java
+++ app/src/main/java/com/jsonapp/doadoresamigos/adapters/DoadorAdapter.java
@@ -1,5 +1,7 @@
 package com.jsonapp.doadoresamigos.adapters;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,16 +10,29 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.jsonapp.doadoresamigos.R;
+import com.jsonapp.doadoresamigos.gestaodoadores.AlteracaoDoador;
+import com.jsonapp.doadoresamigos.gestaodoadores.DoadorDal;
 import com.jsonapp.doadoresamigos.gestaodoadores.DoadorDto;
+import com.jsonapp.doadoresamigos.gestaodoadores.DoadorRepositorioImpl;
+import com.jsonapp.doadoresamigos.gestaodoadores.ExclusaoDoador;
+import com.jsonapp.doadoresamigos.gestaodoadores.ExclusaoDoadorDal;
+import com.jsonapp.doadoresamigos.gestaodoadores.PesquisaDoador;
+import com.jsonapp.doadoresamigos.gestaodoadores.PesquisaDoadorDal;
 
 import java.util.ArrayList;
 
-public class DoadorAdapter extends RecyclerView.Adapter<DoadorViewHolder> {
+public class DoadorAdapter extends RecyclerView.Adapter<DoadorViewHolder> implements ExclusaoDoadorDal {
 
     private final ArrayList<DoadorDto> doadores;
+    private final Context context;
+    private ExclusaoDoador exclusaoDoador;
+    private DoadorViewHolder holder;
+    private DoadorDto doador;
+    private AlteracaoDoador alteracaoDoador;
 
-    public DoadorAdapter(ArrayList<DoadorDto> doadores) {
+    public DoadorAdapter(ArrayList<DoadorDto> doadores, Context context) {
         this.doadores = doadores;
+        this.context = context;
     }
 
     @NonNull
@@ -29,7 +44,7 @@ public class DoadorAdapter extends RecyclerView.Adapter<DoadorViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull DoadorViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull DoadorViewHolder holder, final int position) {
         String nome = doadores.get(position).getNome();
         String id = String.format("Id: %o", doadores.get(position).getCodigo());
         String idade = String.format("Idade: %o anos", doadores.get(position).getIdade());
@@ -41,12 +56,57 @@ public class DoadorAdapter extends RecyclerView.Adapter<DoadorViewHolder> {
         holder.textViewIdade.setText(idade);
         holder.textViewTipoDeSangue.setText(sangue);
         holder.textViewCondicoesFisicas.setText(condicoesFisicas);
+
+        exclusaoDoador = new ExclusaoDoador(this, new DoadorRepositorioImpl(this.context));
+
+        holder.imgViewEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        holder.imgViewDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doador = doadores.get(position);
+                exclusaoDoador.excluirDoador();
+            }
+        });
+
+
     }
-
-
 
     @Override
     public int getItemCount() {
         return doadores.size();
+    }
+
+    @Override
+    public Integer obterCodigoDoador() {
+        return doador.getCodigo();
+    }
+
+    @Override
+    public void notificarCodigoInvalido() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Atenção");
+        builder.setIcon(R.mipmap.ic_alert_atencao);
+        builder.setMessage(R.string.alertdialog_codigoinvalido);
+        builder.create().show();
+    }
+
+    @Override
+    public void notificarUsuarioInexistente() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Atenção");
+        builder.setIcon(R.mipmap.ic_alert_atencao);
+        builder.setMessage(R.string.alertdialog_codigoinexistente);
+        builder.create().show();
+    }
+
+    @Override
+    public void exibirNovaLista(ArrayList<DoadorDto> doadores) {
+        PesquisaDoadorDal pesquisaDoadorDal = (PesquisaDoadorDal) context;
+        pesquisaDoadorDal.exibirDoadores(doadores);
     }
 }
--- app/src/main/java/com/jsonapp/doadoresamigos/gestaodoadores/ExclusaoDoadorDal.java
+++ app/src/main/java/com/jsonapp/doadoresamigos/gestaodoadores/ExclusaoDoadorDal.java
@@ -1,9 +1,13 @@
 package com.jsonapp.doadoresamigos.gestaodoadores;
 
+import java.util.ArrayList;
+
 public interface ExclusaoDoadorDal {
     Integer obterCodigoDoador();
 
     void notificarCodigoInvalido();
 
     void notificarUsuarioInexistente();
+
+    void exibirNovaLista(ArrayList<DoadorDto> doadores);
 }
